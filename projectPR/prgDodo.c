3549c6e08e81037c0c94df5f6ec72253f0dc5822
Nicolas SALLERON <nicolas.salleron@gmail.com>
2017-02-25 18:59:09 +0100

    Version 1.1

--- projectPR/prgDodo.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	sleep(12);
-	printf("HTTP/1.1 200 OK\r\n");
-	printf("Content-Type: text/plain;\r\nCharset=utf-8\r\n");
-	printf("J'ai fini de faire dodo!\r\n");
-}
